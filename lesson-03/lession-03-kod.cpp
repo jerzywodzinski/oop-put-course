9f0dd67d0f1201923aee5d6e93b9603c08b18453
jerzywodzinski <126490205+jerzywodzinski@users.noreply.github.com>
2023-03-20 16:39:08 +0100

    Create lession-03-kod.cpp

---
+++ lesson-03/lession-03-kod.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Book {
+private:
+    string title_;
+    string author_;
+
+public:
+    Book(string title, string author) : title_(title), author_(author) {}
+
+    string Title() { return title_; }
+    string Author() { return author_; }
+
+
+};
+
+class Library {
+
+private:
+    vector<Book> books_;
+
+public:
+    Library(){}
+
+    void addBook(Book book) {
+        books_.push_back(book);
+    }
+
+    void showBooks() {
+        cout << "ksiazki w bibliotece:" << endl;
+        for (int i = 0; i < books_.size(); i++) {
+            cout <<"ksiazka: "<< books_[i].Title() << " autor " << books_[i].Author() << endl;
+        }
+    }
+};
+
+int main() {
+    Library library;
+    Book book1("ksiazka1", "autor1");
+    Book book2("ksiazka2", "autor2");
+    Book book3("ksiazka3", "autor3");
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+    library.showBooks();
+
+    return 0;
+}
